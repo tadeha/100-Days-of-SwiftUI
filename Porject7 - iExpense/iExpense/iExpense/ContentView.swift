0cbdd6c68e135dfca73e0ceb917716f572cd6be7
Tadeh <tadeh19@gmail.com>
2020-04-24 22:20:32 +0430

    Added Day 36 files as ContentViewTest filename in Project7 folder.

---
+++ Porject7 - iExpense/iExpense/iExpense/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  iExpense
+//
+//  Created by Tadeh Alexani on 4/24/20.
+//  Copyright © 2020 Alexani. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+
+  var body: some View {
+    Text("Test")
+  }
+}
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+  }
+}
