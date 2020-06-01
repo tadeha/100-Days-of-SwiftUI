fca6d61db28210f9bb2892ac474366b47cc6d254
Tadeh <tadeh19@gmail.com>
2020-06-01 14:50:48 +0430

    Fixed Guess the Flag Accessibility

--- Project2 - GuessTheFlag/GuessTheFlag/ContentView.swift
+++ Project2 - GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -13,16 +13,32 @@ import SwiftUI
 struct FlagImage: View {
   var name: String
   
+  let labels = [
+      "Estonia": "Flag with three horizontal stripes of equal size. Top stripe blue, middle stripe black, bottom stripe white",
+      "France": "Flag with three vertical stripes of equal size. Left stripe blue, middle stripe white, right stripe red",
+      "Germany": "Flag with three horizontal stripes of equal size. Top stripe black, middle stripe red, bottom stripe gold",
+      "Ireland": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe orange",
+      "Italy": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe red",
+      "Nigeria": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe green",
+      "Poland": "Flag with two horizontal stripes of equal size. Top stripe white, bottom stripe red",
+      "Russia": "Flag with three horizontal stripes of equal size. Top stripe white, middle stripe blue, bottom stripe red",
+      "Spain": "Flag with three horizontal stripes. Top thin stripe red, middle thick stripe gold with a crest on the left, bottom thin stripe red",
+      "UK": "Flag with overlapping red and white crosses, both straight and diagonally, on a blue background",
+      "US": "Flag with red and white stripes of equal size, with white stars on a blue background in the top-left corner"
+  ]
+  
   var body: some View {
     Image(name)
       .renderingMode(.original)
       .clipShape(Capsule())
       .overlay(Capsule().stroke(Color.black, lineWidth: 1))
       .shadow(color: .black, radius: 2)
+      .accessibility(label: Text(self.labels[name] ?? "Unknown Flag"))
   }
 }
 
 struct ContentView: View {
+  
   @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
   @State private var correctAnswer = Int.random(in: 0...2)
   
