317633d1f9e7db1b750dd7bea0720a931dc52833
Tadeh <tadeh19@gmail.com>
2020-06-28 18:35:15 +0430

    Completed Day 98

---
+++ Project19 - SnowSeeker/SnowSeeker/SnowSeeker/Facility.swift
@@ -0,0 +1,9 @@
+//
+//  Facility.swift
+//  SnowSeeker
+//
+//  Created by Tadeh Alexani on 6/28/20.
+//  Copyright © 2020 Alexani. All rights reserved.
+//
+
+import Foundation
---
+++ Project19 - SnowSeeker/SnowSeeker/SnowSeeker/Favorites.swift
@@ -0,0 +1,9 @@
+//
+//  Favorites.swift
+//  SnowSeeker
+//
+//  Created by Tadeh Alexani on 6/28/20.
+//  Copyright © 2020 Alexani. All rights reserved.
+//
+
+import Foundation
